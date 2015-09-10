ab54c6eb8140edd2836e3340829dd47b586ba98b
Chris Wilson <github@yepher.com>
2015-09-10 09:37:56 -0500

    Adds DNSAttributesTest and fixes bug with keynaming

--- libs/sparkpost-api-lib/src/main/java/com/sparkpost/sdk/dto/DNSAttributes.java
+++ libs/sparkpost-api-lib/src/main/java/com/sparkpost/sdk/dto/DNSAttributes.java
@@ -1,5 +1,7 @@
 package com.sparkpost.sdk.dto;
 
+import com.google.gson.annotations.SerializedName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,22 +12,24 @@ public class DNSAttributes extends Base {
 	/**
 	 * DNS DKIM record for the registered Sending Domain
 	 */
+	@SerializedName("dkim_record")
 	private String dkimRecord;
 	
 	/**
 	 * DNS SPF record for the registered Sending Domain
 	 */
+	@SerializedName("spf_record")
 	private String spfRecord;
 	
 	/**
 	 * Error message describing reason for DKIM verification failure
 	 */
+	@SerializedName("dkim_error")
 	private String dkimError;
 	
 	/**
 	 * Error message describing reason for SPF verification failure
 	 */
+	@SerializedName("spf_error")
 	private String spfError;
-	
-	
 }
