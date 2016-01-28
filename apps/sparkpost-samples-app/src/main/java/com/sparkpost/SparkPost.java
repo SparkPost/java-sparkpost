a5de980771ea0845dc2b4fb71fc6e11543819019
Chris Wilson <github@yepher.com>
2016-01-28 14:55:02 -0600

    Adds a simplified API for someone who just wants to send a simple email

---
+++ apps/sparkpost-samples-app/src/main/java/com/sparkpost/SparkPost.java
@@ -0,0 +1,20 @@
+
+package com.sparkpost;
+
+import com.sparkpost.exception.SparkPostException;
+
+public class SparkPost {
+
+    public static void main(String[] args) throws SparkPostException {
+        String API_KEY = "YOUR API KEY HERE!!!";
+        Client client = new Client(API_KEY);
+
+        client.sendMessage(
+                "you@yourdomain.com",
+                "to@sparkpost.com",
+                "The subject of the message",
+                "The text pare of the email",
+                "<b>The HTML part of the email</b>");
+
+    }
+}
