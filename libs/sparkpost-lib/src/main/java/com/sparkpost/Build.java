50c7e117ef08b7db0ad5d500dc24f10a408e2661
Chris Wilson <github@yepher.com>
2016-02-05 12:22:27 -0600

    Fixes version field

--- libs/sparkpost-lib/src/main/java/com/sparkpost/Build.java
+++ libs/sparkpost-lib/src/main/java/com/sparkpost/Build.java
@@ -3,7 +3,7 @@ package com.sparkpost;
 
 public class Build {
 
-    public static final String VERSION = "VERSION_NUMBER";
+    public static final String VERSION = "BUILD_VERSION_NAME";
 
     public static final String BUILD_VERSION = "BUILD_GENERATED_VERSION";
 
