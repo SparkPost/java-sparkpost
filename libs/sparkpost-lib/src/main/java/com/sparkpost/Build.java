15aa7d424fbed43da0c1b9647d80fdf9b5c82f8b
Chris Wilson <github@yepher.com>
2016-02-05 12:19:42 -0600

    Adding more fields to build file

--- libs/sparkpost-lib/src/main/java/com/sparkpost/Build.java
+++ libs/sparkpost-lib/src/main/java/com/sparkpost/Build.java
@@ -3,10 +3,14 @@ package com.sparkpost;
 
 public class Build {
 
-    public static final String VERSION = "0.11";
+    public static final String VERSION = "VERSION_NUMBER";
 
     public static final String BUILD_VERSION = "BUILD_GENERATED_VERSION";
 
     public static final String GIT_HASH = "BUILD_GENERATED_GIT_HASH";
+
+    public static final String GIT_SHORT_HASH = "BUILD_GENERATED_SHORT_GIT_HASH";
+
+    public static final String BUILD_DATE = "BUILD_GENERATED_DATE";
 
 }
