03b482c4c89ac456cc2b074f09bd6605ced9048c
Chris Wilson <github@yepher.com>
2016-02-05 11:53:49 -0600

    Base versioning before introducing release script.

---
+++ libs/sparkpost-lib/src/main/java/com/sparkpost/Build.java
@@ -0,0 +1,12 @@
+
+package com.sparkpost;
+
+public class Build {
+
+    public static final String VERSION = "0.11";
+
+    public static final String BUILD_VERSION = "BUILD_GENERATED_VERSION";
+
+    public static final String GIT_HASH = "BUILD_GENERATED_GIT_HASH";
+
+}
